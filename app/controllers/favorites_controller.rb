a74d6332a5cfdcbbf8791cbada39b2c8afeb1687
keita.kudou <kudoketa0515@yahoo.co.jp>
2018-10-30 11:03:11 +0000

    favorite, application controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,17 @@
 class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+
   def create
+    user = User.find(params[:follow_id])
+    current_user.favorite(micropost)
+    flash[:success] = 'お気に入りしました。'
+    redirect_to user
   end
 
   def destroy
+    user = User.find(params[:follow_id])
+    current_user.unfavorite(micropost)
+    flash[:success] = 'お気に入りを解除しました。'
+    redirect_to user
   end
 end
